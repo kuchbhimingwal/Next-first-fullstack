f37d85b27765494c9c063fb4c518cfe3ea169db4
kuchbhimingwal <shubhammingi@gmail.com>
2024-05-14 23:25:29 -0400

    added a database to teh route!!

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,24 @@
+import { NextRequest } from "next/server";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 export function GET(){
   return Response.json({
     email: "shubhammingi@gmail.com",
     name: "shubham"
   })
 }
+ export async function POST(req: NextRequest){
+  const body = await req.json();
+  console.log(body);
+  await prisma.user.create({
+    data:{
+      username: body.username,
+      password: body.password
+    }
+  })
+
+  return Response.json({
+    message: "you are logged in"
+  })
+ }
