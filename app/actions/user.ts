3e161f6629b78b5a82fd49f2f0e61be46fb4f047
kuchbhimingwal <shubhammingi@gmail.com>
2024-05-16 00:19:28 -0400

    server action added!!!

---
+++ app/actions/user.ts
@@ -0,0 +1,18 @@
+"use server"
+
+import client from "../db"
+
+
+export async function signup (email: string, password :string){
+  try {
+    await client.user.create({
+      data:{
+        username: email,
+        password: password
+      }
+    });
+    return true
+  } catch (error) {
+    return false
+  }
+}
--- app/components/Signup.tsx
+++ app/components/Signup.tsx
@@ -1,6 +1,7 @@
 "use client"
-import axios from "axios";
+
 import { ChangeEventHandler, useState } from "react";
+import { signup } from "../actions/user";
 
 export function Signup() {
     const [username, setUsername] = useState("");
@@ -23,10 +24,7 @@ export function Signup() {
                             setPassword(e.target.value)
                         }} label="Password" type={"password"} placeholder="123456" />
                         <button onClick={()=>{
-                          axios.post("http://localhost:3000/api/user", {
-                            username,
-                            password
-                          });
+                            signup(username, password)
                         }}
                         type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
